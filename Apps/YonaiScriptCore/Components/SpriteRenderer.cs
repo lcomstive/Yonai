b25ca35a3c6838df6efcf90fa92a4345e799b5d5
Lewis Comstive <lcomstive@hotmail.com>
2023-12-12 21:25:50 +1100

    Make SpriteRenderer component C# only

--- Apps/YonaiScriptCore/Components/SpriteRenderer.cs
+++ Apps/YonaiScriptCore/Components/SpriteRenderer.cs
@@ -10,6 +10,7 @@ namespace Yonai
 		/// Texture to draw.
 		/// Sets <see cref="SpriteID"/>.
 		/// </summary>
+		[Serialize(false)]
 		public Texture Sprite
 		{
 			get => Resource.Get<Texture>(SpriteID);
@@ -20,30 +21,18 @@ namespace Yonai
 		/// ID of texture to draw.
 		/// Sets <see cref="Sprite"/>.
 		/// </summary>
-		[Serialize(false)]
-		public UUID SpriteID
-		{
-			get => _GetSprite(Handle);
-			set => _SetSprite(Handle, value);
-		}
+		public UUID SpriteID { get; set; }
 
 		/// <summary>
 		/// Tint to apply to drawn sprite
 		/// </summary>
-		public Colour Colour
-		{
-			get
-			{
-				_GetColour(Handle, out Colour colour);
-				return colour;
-			}
-			set => _SetColour(Handle, ref value);
-		}
+		public Colour Colour { get; set; }
 
 		/// <summary>
 		/// Shader used to draw sprite.
 		/// Sets <see cref="ShaderID"/>.
 		/// </summary>
+		[Serialize(false)]
 		public Shader Shader
 		{
 			get => Resource.Get<Shader>(ShaderID);
@@ -54,20 +43,6 @@ namespace Yonai
 		/// ID of shader used to draw sprite.
 		/// Sets <see cref="Shader"/>.
 		/// </summary>
-		[Serialize(false)]
-		public UUID ShaderID
-		{
-			get => _GetShader(Handle);
-			set => _SetShader(Handle, value);
-		}
-
-		#region Internal Calls
-		[MethodImpl(MethodImplOptions.InternalCall)] private static extern ulong _GetSprite(IntPtr handle);
-		[MethodImpl(MethodImplOptions.InternalCall)] private static extern void  _SetSprite(IntPtr handle, ulong position);
-		[MethodImpl(MethodImplOptions.InternalCall)] private static extern void  _GetColour(IntPtr handle, out Colour colour);
-		[MethodImpl(MethodImplOptions.InternalCall)] private static extern void  _SetColour(IntPtr handle, ref Colour colour);
-		[MethodImpl(MethodImplOptions.InternalCall)] private static extern ulong _GetShader(IntPtr handle);
-		[MethodImpl(MethodImplOptions.InternalCall)] private static extern void  _SetShader(IntPtr handle, ulong shader);
-		#endregion
+		public UUID ShaderID { get; private set; }
 	}
 }
